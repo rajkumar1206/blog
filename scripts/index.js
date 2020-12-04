3f18e45948d5715f6db507e637909adbc4c2e86c
rajkumar <rajkumarbaba12062000@gmail.com>
2020-12-04 09:15:17 +0530

    backend

--- scripts/index.js
+++ scripts/index.js
@@ -40,6 +40,7 @@ function loadjs () {
 }
 
 const url="http://127.0.0.1:8000/";
+const spinnerHandler = document.querySelector(".spinner");
 
 const sendName = document.querySelector('.sendName');
 
